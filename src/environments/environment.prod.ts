4973a14a300044ca4249f01f361bc5255f330ebb
fersaccon <fer.saccon@gmail.com>
2017-10-04 23:15:25 -0600

    Initial Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBfeWTrys1SSS-YGUKK-5UXp9o2iWbM7OI",
+    authDomain: "oshop-b8128.firebaseapp.com",
+    databaseURL: "https://oshop-b8128.firebaseio.com",
+    projectId: "oshop-b8128",
+    storageBucket: "",
+    messagingSenderId: "73597213279"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBfeWTrys1SSS-YGUKK-5UXp9o2iWbM7OI",
+    authDomain: "oshop-b8128.firebaseapp.com",
+    databaseURL: "https://oshop-b8128.firebaseio.com",
+    projectId: "oshop-b8128",
+    storageBucket: "",
+    messagingSenderId: "73597213279"
+  }
 };
